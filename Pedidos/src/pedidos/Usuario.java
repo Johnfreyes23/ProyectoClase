7099efd384cd4f3d639622072d077af96c355f6d
Johnfreyes23 <john_reyes23@hotmail.com>
2017-11-08 14:52:08 -0500

    Prueba

--- Pedidos/src/pedidos/Usuario.java
+++ Pedidos/src/pedidos/Usuario.java
@@ -9,14 +9,6 @@ package pedidos;
  *
  * @author john
  */
-public class Pedidos {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-        System.out.println("Holi");
-    }
+public class Usuario {
     
 }
