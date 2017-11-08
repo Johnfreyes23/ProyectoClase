95af083962f260dc47f15b1bab3ad8fd6de32d42
Johnfreyes23 <john_reyes23@hotmail.com>
2017-11-08 15:48:58 -0500

    Creada Clase Producto

--- Pedidos/src/pedidos/Producto.java
+++ Pedidos/src/pedidos/Producto.java
@@ -10,5 +10,49 @@ package pedidos;
  * @author john
  */
 public class Producto {
+
+    private float alto;
+    private float ancho;
+    private float area;
+    private float valor;
+
+    public Producto(float alto, float ancho ) {
+        this.alto = alto;
+        this.ancho = ancho;
+    }
+    
+    
+
+    public float getAlto() {
+        return alto;
+    }
+
+    public void setAlto(float alto) {
+        this.alto = alto;
+    }
+
+    public float getAncho() {
+        return ancho;
+    }
+
+    public void setAncho(float ancho) {
+        this.ancho = ancho;
+    }
+
+    public float getArea() {
+        return area;
+    }
+
+    public void setArea(float area) {
+        this.area = area;
+    }
+
+    public float getValor() {
+        return valor;
+    }
+
+    public void setValor(float valor) {
+        this.valor = valor;
+    }
     
 }
