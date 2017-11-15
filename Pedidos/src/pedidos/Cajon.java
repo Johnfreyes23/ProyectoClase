4aba912153dca4802f1248f15d59d29c2da73686
Johnfreyes23 <john_reyes23@hotmail.com>
2017-11-14 20:51:15 -0500

    Se crean funciones en la clase Cajon para calcular el precio del producto

---
+++ Pedidos/src/pedidos/Cajon.java
@@ -0,0 +1,108 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pedidos;
+
+/**
+ *
+ * @author john
+ */
+public class Cajon extends Producto {
+    
+    private int tubos;
+    private int leds;
+    private double canto;
+    private double manofactura;
+    private double perimetro;
+    private double otros;
+    private Acrilico frente;
+    private Acrilico cantonera;
+
+    public Cajon(int tubos , int leds, double canto, double manofactura,double otros , double alto, double ancho) {
+        super(alto, ancho);
+        this.tubos = tubos;
+        this.leds = leds;
+        this.canto = canto;
+        this.manofactura = manofactura;
+        this.otros = otros;
+        perimetro = 2*alto+2*ancho;
+        frente = new Acrilico(3,"otros",alto,ancho);
+        cantonera = new Acrilico(3,"otros",canto,perimetro);
+    }
+    public double valorEstructura() {
+        if (getAlto() > getAncho()) {
+            perimetro = (int)(2 * getAlto() + 5 * getAncho()) - 1;
+        } else {
+            perimetro = (int)(5 * getAlto() + 2 * getAncho()) - 1;
+        }
+
+        double aluminio = (int)(perimetro / 600) + 1;
+        return aluminio * 54000;
+    }
+    public double CalcularPrecio() {
+        double valor;
+        double costoLuz = tubos*5000 + leds*4500;  
+        valor = costoLuz + frente.CalcularPrecio() + cantonera.CalcularPrecio()+ valorEstructura()+otros;
+        setValor(valor);
+        return valor;
+    }
+
+    public Acrilico getFrente() {
+        return frente;
+    }
+
+    public void setFrente(Acrilico frente) {
+        this.frente = frente;
+    }
+
+    public Acrilico getCantonera() {
+        return cantonera;
+    }
+
+    public void setCantonera(Acrilico cantonera) {
+        this.cantonera = cantonera;
+    }
+    
+    public int getTubos() {
+        return tubos;
+    }
+
+    public void setTubos(int tubos) {
+        this.tubos = tubos;
+    }
+
+    public int getLeds() {
+        return leds;
+    }
+
+    public void setLeds(int leds) {
+        this.leds = leds;
+    }
+
+    public double getCanto() {
+        return canto;
+    }
+
+    public void setCanto(double canto) {
+        this.canto = canto;
+    }
+
+    public double getManofactura() {
+        return manofactura;
+    }
+
+    public void setManofactura(double manofactura) {
+        this.manofactura = manofactura;
+    }
+
+    public double getPerimetro() {
+        return perimetro;
+    }
+
+    public void setPerimetro(double perimetro) {
+        this.perimetro = perimetro;
+    }
+
+}
--- Pedidos/src/pedidos/Main.java
+++ Pedidos/src/pedidos/Main.java
@@ -9,7 +9,7 @@ package pedidos;
  *
  * @author john
  */
-public class Pedido {
+public class Main {
 
     /**
      * @param args the command line arguments
@@ -17,7 +17,8 @@ public class Pedido {
     public static void main(String[] args) {
         // TODO code application logic here        
         Acrilico a1  = new Acrilico(4,"cristal",30,40);
-        System.out.println(a1.CalcularPrecio());
+        Cajon c1 = new Cajon(6,0,3,0,0,100,50);
+        System.out.println(c1.CalcularPrecio());
         System.out.println(a1.getCosto());
     }
     
