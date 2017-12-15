b0ae01161109070e21500127bd8e592ef95c0a01
Nicolas <Nicolas@Nicols-Gamboa>
2017-12-15 02:22:47 -0500

    Vista Produccion terminada

--- Pedidos/src/Ventanas/VistaFactura.java
+++ Pedidos/src/Ventanas/VistaFactura.java
@@ -59,6 +59,9 @@ int i =0;
             });
         }
         Tabla.setModel(table);
+        Tabla.getColumn("Item").setPreferredWidth(50);
+        Tabla.getColumn("Descripción").setPreferredWidth(435);
+        Tabla.getColumn("Valor").setPreferredWidth(108);
     }
 
     /**
